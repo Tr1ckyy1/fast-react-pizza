27196a9179a45dc90ee95d7e5e7c1d55763cd59a
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2023-09-05 18:42:28 +0400

    .

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppLayout />} errorElement={<Error />}>
+      <Route index element={<Home />} />
+      <Route
+        path="menu"
+        element={<Menu />}
+        loader={menuLoader}
+        errorElement={<Error />}
+      />
+      <Route path="cart" element={<Cart />} />
+      <Route
+        path="order/new"
+        element={<CreateOrder />}
+        action={createOrderAction}
+      />
+      <Route
+        path="order/:orderId"
+        element={<Order />}
+        loader={orderLoader}
+        errorElement={<Error />}
+      />
+    </Route>,
+  ),
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
